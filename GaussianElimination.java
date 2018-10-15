4b44dc1312e1e467a6a05f00de5b23c4de11006d
Michael Trinh <mptrinh@cpp.edu>
2018-10-15 00:26:40 -0700

    first commit

---
+++ GaussianElimination.java
@@ -0,0 +1,19 @@
+/**
+ * Michael Trinh
+ * CS 3010
+ * Programming Project 1
+ *  Description: Write a program that asks the user for the number of linear 
+ *   equations to solve (let’s say n <=10) using the following three methods.
+ */
+
+public class GaussianElimination {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Scaled Partial Pivoting, Jacobi Iterative, "
+                + "Gauss-Seidel");
+    }
+    
+}
